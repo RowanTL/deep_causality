d39c10bb07f85724532631904aed15c38a691789
Rowan Torbitzky-Lane <rowan.a.tl@protonmail.com>
2026-07-05 13:29:12 -0700

    add flake.* files

---
+++ flake.nix
@@ -0,0 +1,56 @@
+{
+  description = "Rust env nix flake with Rust Rover";
+
+  inputs = {
+    nixpkgs.url      = "github:NixOS/nixpkgs/nixos-unstable";
+    rust-overlay.url = "github:oxalica/rust-overlay";
+    flake-utils.url  = "github:numtide/flake-utils";
+  };
+
+  outputs = {nixpkgs, rust-overlay, flake-utils, ... }:
+    flake-utils.lib.eachDefaultSystem (system:
+      let
+        overlays = [ (import rust-overlay) ];
+        pkgs = import nixpkgs {
+          inherit system overlays;
+          config.allowUnfree = true;
+        };
+
+        rustToolchain = 
+          (pkgs.rust-bin.stable.latest.default.override {
+            extensions = [ "rust-analyzer" "rust-src" ];
+          });
+      in
+      {
+        devShells.default = pkgs.mkShell {
+          nativeBuildInputs = [
+            rustToolchain
+          ];
+
+          buildInputs = with pkgs; [
+            openssl
+            pkg-config
+            jetbrains.rust-rover
+            curl
+
+            # cargo programs
+            cargo-nextest
+            cargo-outdated
+            cargo-udeps
+            cargo-audit
+            cargo-machete
+          ];
+
+          shellHook = ''
+            mkdir -p ~/.rust-rover/toolchain
+
+            ln -sfn ${rustToolchain}/lib ~/.rust-rover/toolchain
+            ln -sfn ${rustToolchain}/bin ~/.rust-rover/toolchain
+
+            export RUST_SRC_PATH="$HOME/.rust-rover/toolchain/lib/rustlib/src/rust/library"
+          '';
+        };
+      }
+    );
+}
+
